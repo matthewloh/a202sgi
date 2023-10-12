c682b1dafb55f71caa68745fc886b6948150183e
mch3pg4 <mch3pg4@gmail.com>
2023-10-12 18:48:37 +0800

    added booking fragments

---
+++ app/src/main/java/com/example/intisuperapp/BookingsFragment.java
@@ -0,0 +1,54 @@
+package com.example.intisuperapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.example.intisuperapp.LoginAndRegistration.RegistrationFragment;
+import com.example.intisuperapp.databinding.FragmentBookingsBinding;
+import com.example.intisuperapp.databinding.FragmentRegistrationBinding;
+
+
+public class BookingsFragment extends Fragment {
+
+    private FragmentBookingsBinding binding;
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentBookingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.createABookingButton.setOnClickListener(
+                v -> {
+                    NavHostFragment.findNavController(BookingsFragment.this)
+                            .navigate(R.id.action_bookingsFragment_to_createBookings);
+                }
+        );
+
+        binding.seeAllText.setOnClickListener(
+                v -> {
+                    NavHostFragment.findNavController(BookingsFragment.this)
+                            .navigate(R.id.action_bookingsFragment_to_showAllBookings);
+                }
+        );
+}
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
