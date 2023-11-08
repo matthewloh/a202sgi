dcf7f3fe549fb59d441e7852be1151f9c8125e73
P20012706 <p20012706@student.newinti.edu.my>
2023-11-08 11:55:05 +0800

    updated nav_graph and some elements on the xml layouts for lost&found

--- app/src/main/java/com/example/intisuperapp/LostAndFound/LostAndFoundFragment.java
+++ app/src/main/java/com/example/intisuperapp/LostAndFound/LostAndFoundFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,19 +28,11 @@ public class LostAndFoundFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.lostAndFoundMainPage.foundAnItem.setOnClickListener(
-                v -> {
-                    binding.lostAndFoundMainPage.getRoot().setVisibility(View.INVISIBLE);
-                    binding.lostAndFoundItemLost.getRoot().setVisibility(View.INVISIBLE);
-                    binding.lostAndFoundItemFound.getRoot().setVisibility(View.VISIBLE);
-                }
+        binding.itemfoundbtn.setOnClickListener(
+                v -> NavHostFragment.findNavController(LostAndFoundFragment.this).navigate(R.id.action_lostAndFoundFragment_to_lostAndFoundReportFoundFragment)
         );
-        binding.lostAndFoundMainPage.reportLostItem.setOnClickListener(
-                v -> {
-                    binding.lostAndFoundMainPage.getRoot().setVisibility(View.INVISIBLE);
-                    binding.lostAndFoundItemLost.getRoot().setVisibility(View.VISIBLE);
-                    binding.lostAndFoundItemFound.getRoot().setVisibility(View.INVISIBLE);
-                }
+        binding.itemlostbtn.setOnClickListener(
+                v -> NavHostFragment.findNavController(LostAndFoundFragment.this).navigate(R.id.action_lostAndFoundFragment_to_lostAndFoundReportLostFragment)
         );
     }
 
